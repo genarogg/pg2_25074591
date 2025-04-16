ebdc8f3d623868361a044644272b7c382da27b40
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2025-04-17 03:27:25 +0800

    antes de eliminar

--- index.ts
+++ index.ts
@@ -2,18 +2,42 @@
 import clear from "console-clear";
 clear();
 
+// Importaciones principales
 import express from 'express';
+import path from 'path';
+import cors from "cors";
+import { log } from "@fn";
+import { inicioRouter } from "@router";
 
+// Configuración de la aplicación
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-import middlewareBasicos from "@config/middlewareBasicos";
-middlewareBasicos(app);
+// Middleware básicos
+app.use(express.static(path.join(__dirname, "./src/public")));
 
-import viewEngine from "@config/viewEngine";
-viewEngine(app);
+// Usa cors como middleware
+const CORS_URL = process.env.CORS_URL || "*";
+app.use(cors({ origin: CORS_URL }));
 
-import router from "@config/router";
-router(app);
+// Middleware para analizar el cuerpo de las solicitudes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-import startServer from "@config/startServer";
-startServer(app);
+// Configuración del motor de vistas
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "./src/views"));
+
+// Configuración de rutas
+app.use("/", inicioRouter);
+
+// Middleware de manejo de errores
+app.use((err: any, req: any, res: any, next: any) => {
+    console.error(err.stack);
+    res.status(500).send("¡Algo salió mal!");
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    log.success(`El servidor está corriendo en http://localhost:${PORT}`);
+});
--- src/config/viewEngine.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-const viewEngine = (app: any) => {
-    app.set('view engine', 'ejs');
-    app.set("views", path.join(__dirname, "../views"))
-}
-
-export default viewEngine;
